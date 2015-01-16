edc83bb8673e60b8cadf997febbce4e56796c8e9
Aeluin Chen <aeluin.chen@moxa.com>
2015-01-16 14:27:28 +0800

    Update hellosanji for easy to demo.

--- 01-hellosanji/hellosanji.py
+++ 01-hellosanji/hellosanji.py
@@ -5,79 +5,28 @@ import logging
 import os
 from sanji.core import Sanji
 from sanji.core import Route
-from sanji.model_initiator import ModelInitiator
 from sanji.connection.mqtt import Mqtt
-
-from voluptuous import Schema
-from voluptuous import Required
-from voluptuous import All
-from voluptuous import Length
 
 
 class Hellosanji(Sanji):
 
     def init(self, *args, **kwargs):
-        path_root = os.path.abspath(os.path.dirname(__file__))
-        self.model = ModelInitiator("hellosanji", path_root)
         self.message = "Hello Sanji!"
 
     @Route(methods="get", resource="/hellosanji")
     def get(self, message, response):
+        return response(data="message": self.message)
 
-        if "collection" in message.query:
-            if message.query["collection"] == "true":
-                # collection=true
-                return response(data=self.model.db)
-
-        if "id" in message.param:
-            rsp_msg = None
-            for item in self.model.db["collection"]:
-                if item["id"] == message.param["id"]:
-                    # information of specific id
-                    rsp_msg = item["message"]
-
-            return response(data=rsp_msg)
-
-        # capability
-        id_list = []
-        for item in self.model.db["collection"]:
-            id_list.append(item["id"])
-        return response(data=id_list)
-
-    @Route(methods="put", resource="/hellosanji")
+    @Route(methods=["post", "put"], resource="/hellosanji")
     def put(self, message, response):
-        if hasattr(message, "data"):
-            self.message = message.data["message"]
-            return response()
-        return response(code=400, data={"message": "Invaild Input."})
+        self.message = message.data["message"]
+        return response()
 
-    # See more example about schema define:
-    # Note: This feature is not supports unittest now
-    #   https://github.com/alecthomas/voluptuous
-    post_schema = Schema({
-        Required("message"): All(str, Length(min=1, max=20))
-    })
-
-    @Route(methods="post", resource="/hellosanji", schema=post_schema)
-    def post(self, message, response):
-        if hasattr(message, "data"):
-            self.message = {"id": 53}
-            return response(data=self.message)
-        return response(code=400, data={"message": "Invalid Post Input."})
-
-    @Route(methods="delete", resource="/hellosanji/:id")
+    @Route(methods="delete", resource="/hellosanji")
     def delete(self, message, response):
+        self.message = ""
+        return response(data=self.message)
 
-        if "id" in message.param:
-            self.message = "delete index: %s" % message.param["id"]
-            return response()
-
-        return response(code=400, data={"message": "Invalid Delete Input."})
-
-    @Route(methods="get", resource="/hellosanji/:id/:uid")
-    def get_uid(self, message, response):
-        msg = "id: %s, uid: %s" % (message.param["id"], message.param["uid"])
-        return response(data={"message": msg})
 
 if __name__ == "__main__":
     FORMAT = "%(asctime)s - %(levelname)s - %(lineno)s - %(message)s"
